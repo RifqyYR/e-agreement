395e58ade08eadd399b925142e17491c6e44d50c
RifqyYR <yudhiestrarifqy@gmail.com>
2023-09-21 09:14:52 +0800

    improve delete feature

--- app/Http/Controllers/AgreementController.php
+++ app/Http/Controllers/AgreementController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Models\Agreement;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\File;
-use Illuminate\Support\Facades\Storage;
 use Ramsey\Uuid\Uuid;
 
 class AgreementController extends Controller
--- resources/views/includes/script.blade.php
+++ resources/views/includes/script.blade.php
@@ -138,7 +138,7 @@
                                         d="M362.7 19.3L314.3 67.7 444.3 197.7l48.4-48.4c25-25 25-65.5 0-90.5L453.3 19.3c-25-25-65.5-25-90.5 0zm-71 71L58.6 323.5c-10.4 10.4-18 23.3-22.2 37.4L1 481.2C-1.5 489.7 .8 498.8 7 505s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L421.7 220.3 291.7 90.3z" />
                                 </svg>
                             </button></a>
-                        <a href="{{ url('/delete/` + res.agreements[i].id +`') }}"><button type="button" class="btn btn-danger btn-sm">
+                            <button type="button" class="btn btn-danger btn-sm" data-toggle="modal" data-target="#deleteModal">
                                 <svg xmlns="http://www.w3.org/2000/svg" height="1em"
                                     viewBox="0 0 448 512">
                                     <style>
@@ -149,7 +149,7 @@
                                     <path
                                         d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z" />
                                 </svg>
-                            </button></a>
+                            </button>
                     </div>
                 </td>
           </tr>`;
@@ -161,13 +161,11 @@
 {{-- Alert --}}
 <script>
     //message with toastr
-    @if(session()->has('success'))
-    
-        toastr.success('{{ session('success') }}', 'BERHASIL!'); 
+    @if (session()->has('success'))
 
-    @elseif(session()->has('error'))
+        toastr.success('{{ session('success') }}', 'BERHASIL!');
+    @elseif (session()->has('error'))
 
-        toastr.error('{{ session('error') }}', 'GAGAL!'); 
-        
+        toastr.error('{{ session('error') }}', 'GAGAL!');
     @endif
 </script>
